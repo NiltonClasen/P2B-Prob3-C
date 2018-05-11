e71d195e4b3762bba344b8b48f5589c201e4c0d4
danielborbavareladossantos <danielvarelaborba@gmail.com>
2018-05-11 19:00:27 -0300

    first commit

---
+++ src/com/Problema3/Pedido.java
@@ -0,0 +1,63 @@
+package com.Problema3;
+
+import java.util.ArrayList;
+import java.util.Date;
+import com.Problema3.ItemPedido;
+import com.Problema3.Produto;
+
+public class Pedido {
+
+    private int numero;
+    private String nomeCliente;
+    private Date data;
+    private String endereco;
+    private ArrayList<ItemPedido> itens;
+
+    public Pedido() {
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getNomeCliente() {
+        return nomeCliente;
+    }
+
+    public void setNomeCliente(String nomeCliente) {
+        this.nomeCliente = nomeCliente;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public void incluirItem(Produto p, int qtd) {
+        this.itens.add(new ItemPedido(p, qtd));
+    }
+
+    public double getValorPedido() {
+        double valorTotal = 0;
+        for (ItemPedido ip : this.itens) {
+            valorTotal += (ip.getValorItem());
+        }
+        return valorTotal;
+    }
+
+}
